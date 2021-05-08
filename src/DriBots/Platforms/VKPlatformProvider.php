7cabb50eb640f3d8dcb3107ba6a654be28f1e372
levkopo <40952805+levkopo@users.noreply.github.com>
2021-05-08 21:26:17 +0500

    1.0.3 support

--- src/DriBots/Platforms/VKPlatformProvider.php
+++ src/DriBots/Platforms/VKPlatformProvider.php
@@ -4,8 +4,13 @@
 namespace DriBots\Platforms;
 
 
+use CURLFile;
+use DriBots\Data\Attachment;
+use DriBots\Data\Attachments\PhotoAttachment;
 use DriBots\Data\Message;
+use DriBots\Data\User;
 use JsonException;
+use stdClass;
 
 class VKPlatformProvider implements BasePlatformProvider {
     public function __construct(
@@ -15,19 +20,84 @@ class VKPlatformProvider implements BasePlatformProvider {
     /**
      * @throws JsonException
      */
-    public function sendMessage(int $toId, string $text): Message|false {
+    public function sendMessage(int $toId, string $text, Attachment $attachment = null): Message|false {
         if($messageData = $this->call("messages.send", [
             "peer_ids" => $toId,
             "message" => $text,
-            "random_id" => 0
+            "random_id" => 0,
+            "attachment" => $this->uploadAttachment($toId, $attachment)
         ])){
             $messageData = $messageData[0];
             if(!isset($messageData["error"])) {
                 return new Message(
                     id: $messageData['conversation_message_id']??$messageData['message_id'],
                     fromId: $this->platform->groupId,
-                    text: $text);
+                    text: $text,
+                    attachment: $attachment
+                );
             }
+        }
+
+        return false;
+    }
+
+    /**
+     * @throws JsonException
+     */
+    private function uploadAttachment(int $peerId, ?Attachment $attachment){
+        $response = "";
+        if($attachment instanceof PhotoAttachment){
+            $photo = $this->uploadPhoto($peerId, $attachment);
+            $response = "photo".$photo[0]['owner_id']."_".$photo[0]['id'];
+        }
+
+        return $response;
+    }
+
+    /**
+     * @throws JsonException
+     */
+    private function uploadPhoto(int $peer_id, PhotoAttachment $file): array{
+        $response = $this->call('photos.getMessagesUploadServer', array(
+            'peer_id' => $peer_id,
+        ));
+
+        $upload_response = $this->upload($response['upload_url'], $file->path);
+        return $this->call('photos.saveMessagesPhoto', array(
+            'photo' => $upload_response['photo'],
+            'server' => $upload_response['server'],
+            'hash' => $upload_response['hash'],
+        ));
+    }
+
+    /**
+     * @throws JsonException
+     */
+    private function upload(string $url, string $file): array {
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, array('file' => new CURLfile(realpath($file))));
+        $json = curl_exec($curl);
+        curl_close($curl);
+
+        return json_decode($json, true, 512, JSON_THROW_ON_ERROR);
+    }
+
+    /**
+     * @throws JsonException
+     */
+    public function getUser(int $userId): User|false {
+        if($userData = $this->call("users.get", [
+            "user_ids"=>$userId,
+            "fields"=>"domain"
+        ])){
+            $user = $userData[0];
+
+            return new User(
+                id: $user['id'],
+                username: $user['domain']
+            );
         }
 
         return false;
